a4f0e090c8ece8b1589a876b597f70213ec1d4e8
Niels Verheijen <kahlan147@gmail.com>
2019-03-04 20:53:15 +0100

    Simple message send/receive

--- mix/messaging/MessageSender.java
+++ mix/messaging/MessageSender.java
@@ -1,7 +1,36 @@
 package messaging;
 
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 /**
  * Created by Niels Verheijen on 04/03/2019.
  */
 public class MessageSender {
+
+    private final String QUEUE_NAME = "test";
+
+    public MessageSender(){
+        setup();
+    }
+
+    private void setup(){
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+        catch (TimeoutException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
